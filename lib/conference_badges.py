74df6d9efe008b8c9ae5636324ac7077478bca57
Christian Garcia <bones192nd@gmail.com>
2023-05-25 13:06:38 -0400

    lab submission

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,18 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = [badge_maker(name) for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    room = list()
+    # for name in names:
+    #     room.append(f"Hello, {name}! You'll be assigned to room {names.index(name) +1}!" )
+    for index, name in enumerate(names):
+        room.append(f"Hello, {name}! You'll be assigned to room {index +1}!" )
+    return room
 
 def printer(names):
-    return None
+    [print(item) for item in batch_badge_creator(names)]
+    [print(item) for item in assign_rooms(names)]
